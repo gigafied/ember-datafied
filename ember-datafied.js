dfd12ba57c8eba88f2e09cbdcb39a25d089fe2b6
Taka Kojima <taka@gigafied.com>
2015-03-10 14:38:25 -0700

    New build (VERSION 0.4.3)

--- ember-datafied.js
+++ ember-datafied.js
@@ -4,14 +4,14 @@
  * @author      gigafied (Taka Kojima)
  * @repo        https://github.com/gigafied/ember-datafied
  * @license     Licensed under MIT license
- * @VERSION     0.4.2
+ * @VERSION     0.4.3
  */
 ;(function (global) {
 
 "use strict";
 
 var DF = global.DF = Ember.Namespace.create({
-    VERSION : '0.4.2'
+    VERSION : '0.4.3'
 });
 
 DF.required = function (message) {
